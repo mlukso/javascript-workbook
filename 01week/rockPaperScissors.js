6bb84c2ab6c864d9b309a6046ea50926d226bb72
Matt Lukso <mk.lukso@gmail.com>
2018-07-19 22:50:07 -0500

    whiteboard RPS (no code)

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,8 +9,27 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
   // Write code here
+
+  // 1. Did the users enter a valid input? Need a function to check if hand1 & hand2 are valid. If not, return error. I can write a condition to check the string for 'rock', 'paper', or 'scissors'
+  // 2. Will need conditions for both hand1 and hand2.
+  // 3. If they are valid, I need to write conditions to compare the inputs.
+  // 4. Write a condition to check: if (hand1 === 'rock' && hand2 === 'scissors') return "Hand one wins!"
+  // 5. Write a condition to check: if (hand1 === 'rock' && hand2 === 'paper') return "Hand two Wins!"
+  // 6. Write a condition to check: if (hand1 === 'rock' && hand2 === 'rock') return "It's a tie!"
+  // 7. Write a condition to check: if (hand1 === 'paper' && hand2 === 'rock') return "Hand one wins!"
+  // 8. Write a condition to check: if (hand1 === 'paper' && hand2 === 'scissors') return "Hand two wins!"
+  // 9. Write a condition to check: if (hand1 === 'paper' && hand2 === 'paper') return "It's a tie!"
+  // 10. Write a condition to check: if (hand1 === 'scissors' && hand2 === 'paper') return "Hand one wins!"
+  // 11. Write a condition to check: if (hand1 === 'scissors' && hand2 === 'rock') return "Hand two wins!"
+  // 12. Write a condition to check: if (hand1 === 'scissors' && hand2 === 'scissors') return "It's a tie!"
+
+  // Can you think of a simpler way?
+  // Still need a condition to check if the inputs are valid for hand1 and hand2. Is it a string? Does it match 'rock', 'paper', or 'scissors'? If not, return "Please choose type 'rock', 'paper', or 'scissors'." and request input again. 
+  // else if (hand1 === hand2) then return "It's a tie!"
+  // else if (hand1 === 'rock' && hand2 === 'paper') return "Hand two wins!" else return "Hand one wins!"
+  // else if (hand1 === 'paper' && hand2 === 'rock') return "Hand two wins!" else return "Hand one wins!"
+  // else if (hand1 === 'scissors' && hand2 === 'rock') return "Hand two wins!" else return "Hand one wins!"
 
 }
 

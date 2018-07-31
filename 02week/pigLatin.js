ad8c4586d16e692e5fd954f079d2566577b8fbdf
Matt Lukso <mk.lukso@gmail.com>
2018-07-30 23:38:16 -0500

    first try at pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,17 +7,81 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Check if the input exists, is a string, and isn't a number
+// Trim any spaces and set characters to lowercase
+// Break up the sting into an array with .split()
+// Check each value of array for a vowel
+// If first letter is a vowel, return and add 'yay'
+// If not, find first vowel
+// Use new array and push values from the original array until first vowel
+// Use vowel location to parse the original array to remove values up to first vowel
+// Return both arrays joined into a new string and add 'ay'
 
-function pigLatin(word) {
+// TODOS:
+// Find way to check for any characters that are not letters and throw error or remove them (no regex)
 
-  // Your code here
+// Function that checks for vowels in an array
+const checkForVowel=(array, index)=>{
+  return array[index] === 'a' || array[index] === 'e' || array[index] === 'i' || array[index] === 'o' || array[index] === 'u'
+}
 
+// Function to clean up the input by trimming spaces and converting to all lowercase characters
+const wordLowerCaseTrim=(input)=>{
+  return input.trim().toLowerCase().split("")
+  }
+
+// Function to check for 'y' as vowel. Doesn't work if 'y' is the first letter (probably needs some additional work because 'y' is weird)
+const checkForY=(array, index)=>{
+  if(array[0] !== 'y'){
+    return array[index] === 'y'
+  }
+}
+
+const pigLatin=(word)=>{
+  if(!word || typeof word !== 'string' || !isNaN(word)){
+    return "Please enter a valid word"
+  }
+  else {
+    const originalWordArray = wordLowerCaseTrim(word)
+    const lettersToParseArray = [];
+    let firstVowelIndex;
+
+    // If first letter is vowel (but not 'y'), immedately just add 'yay' and return
+    if(checkForVowel(originalWordArray, 0)){
+      return originalWordArray.join('') + 'yay'
+    }
+
+    // Find the location of the first vowel (or 'y' if no other vowel is found)
+    for (let i=0; i < originalWordArray.length; i++){
+      if(checkForVowel(originalWordArray, i)){
+          firstVowelIndex = i; break;
+      } else if (checkForY(originalWordArray, i)){
+          firstVowelIndex = i; break;
+      }
+    }
+
+    // Push letters from original word until first vowel to new array
+    for (let j=0; j < firstVowelIndex; j++){
+      lettersToParseArray.push(originalWordArray[j])
+    }
+
+    // Remove letters from original array until first vowel
+      originalWordArray.splice(0, firstVowelIndex);
+
+    // Combine two arrays into a new word, add 'ay'
+        return originalWordArray.join('') + lettersToParseArray.join('') + 'ay'
+  }
 }
 
 
+// Function that converts all words in a sentence to pigLatin
+const pigLatinSentence=(sentence)=>{
+  return sentence.trim().split(' ').map(word => pigLatin(word)).join(' ')
+}
+
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }

48888dcbbe4e310284274de0f140bc132f133cfd
Matt Lukso <mk.lukso@gmail.com>
2018-08-05 14:20:47 -0500

    added some tests

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,44 +19,43 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-const movePiece=(input1, input2)=> {
-return stacks[input2].push(stacks[input1].pop(input1))
+const movePiece = (input1, input2) => {
+  return stacks[input2].push(stacks[input1].pop(input1))
 }
 
-const isValid=(input1, input2)=> {
-  const validInputs = ['a', 'b', 'c']
-  const inputsToArray = input1.concat(input2).split('')
-  return inputsToArray.every(input => validInputs.includes(input))
-}
-
-const isLegal=(input1, input2)=> {
-   if ((stacks[input1].length > 0)
-    && (stacks[input1].slice(-1) < stacks[input2].slice(-1))
-    || (stacks[input2].length == 0)
-    && (input1 !== input2)) {
-      return true
-   } else {
-      return false
-   }
-}
-
-const checkForWin=()=> {
-  if (stacks.b.length === 4 || stacks.c.length === 4){
-    return true
-  } else {
-    return false
+const isValid = (input1, input2) => {
+  if (input1 && input2) {
+    const combinedInputs = input1 += input2
+    const combinedInputsToArray = (input1 += input2).split('')
+    return combinedInputsToArray.every(input => ['a', 'b', 'c'].includes(input))
   }
 }
 
-const resetGame=()=> {
-  stacks = {
-      a: [4, 3, 2, 1],
-      b: [],
-      c: []
-    };
+const isLegal = (input1, input2) => {
+  return stacks[input1].length > 0 &&
+    stacks[input1].slice(-1) < stacks[input2].slice(-1) ||
+    stacks[input2].length === 0 &&
+    input1 !== input2
 }
 
-const towersOfHanoi=(startStack, endStack)=> {
+const checkForWin = () => {
+  return stacks.b.length === 4 || stacks.c.length === 4
+}
+
+const checkForReset = (input1, input2) => {
+  const combinedInputsToCheckForReset = input1 += input2
+  return combinedInputsToCheckForReset.includes('reset')
+}
+
+const resetGame = () => {
+  stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+  };
+}
+
+const towersOfHanoi = (startStack, endStack) => {
 
   const startStackFormatted = startStack.trim().toLowerCase();
   const endStackFormatted = endStack.trim().toLowerCase();
@@ -66,17 +65,17 @@ const towersOfHanoi=(startStack, endStack)=> {
 
       movePiece(startStackFormatted, endStackFormatted);
 
-      if (checkForWin()){
+      if (checkForWin()) {
         console.log('Winner! Enter "reset" to play again.')
         return
+      }
+    } else {
+      console.log('Invalid move. Try again.')
     }
-  } else {
-    console.log('Invalid move. Try again.')
-  }
-} else if (startStackFormatted.concat(endStackFormatted).includes('reset')) {
+  } else if (checkForReset(startStackFormatted, endStackFormatted)) {
     console.log('Game has been reset!')
     resetGame();
-      return
+    return
   } else {
     console.log('Invalid input. Try again.')
   }
@@ -99,7 +98,11 @@ if (typeof describe === 'function') {
   describe('#towersOfHanoi()', () => {
     it('should be able to move a block', () => {
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      });
     });
   });
 
@@ -121,12 +124,48 @@ if (typeof describe === 'function') {
       assert.equal(isLegal('a', 'c'), true);
     });
   });
+
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      stacks = {
+        a: [],
+        b: [4, 3, 2, 1],
+        c: []
+      };
       assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
+      stacks = {
+        a: [1],
+        b: [4, 3, 2],
+        c: []
+      };
       assert.equal(checkForWin(), false);
+    });
+  });
+
+  describe('#isValid()', () => {
+    it('should not allow an invlaid move', () => {
+      assert.equal(isValid(1, 'b'), false);
+    });
+    it('should allow a valid move', () => {
+      assert.equal(isValid('a', 'b'), true);
+    });
+  });
+
+  describe('#checkForReset()', () => {
+    it('should detect a reset request', () => {
+      assert.equal(checkForReset('a', 'reset'), true);
+      assert.equal(checkForReset('a', 'c'), false);
+    });
+  });
+
+  describe('#reset()', () => {
+    it('should reset game', () => {
+      towersOfHanoi('reset', 'a');
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      });
     });
   });
 
